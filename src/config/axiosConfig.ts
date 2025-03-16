2736318b2e4fe7a7c2b939b5939a0190f43b1721
mohamad727 <mahmodim222@gmail.com>
2025-03-17 01:37:03 +0330

    url server

--- src/config/axiosConfig.ts
+++ src/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://jsonserver-peach.vercel.app/', // آدرس سرور شما
+  baseURL: 'http://localhost:5000/', // آدرس سرور شما
   timeout: 10000, // Timeout به مدت 10 ثانیه
 });
 
